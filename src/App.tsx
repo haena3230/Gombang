b8332e739f291df3a63534291ba39c6ebd00d5ac
haena3230 <gosk0912@naver.com>
2020-08-15 15:43:53 +0900

    ready

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+// App.tsx test
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+// import TestExample from './NativeModule/TestExample';
+
+const App = () => {
+  return <SafeAreaView>{/* <TestExample /> */}</SafeAreaView>;
+};
+export default App;
