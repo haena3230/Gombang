1307faa93fc12f4f4fadca5a1598ae60a713b3f3
Gongcu <knmy0101@naver.com>
2020-11-21 00:06:52 +0900

    FCM

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
+
+//FCM
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+    //remoteMessage에 서버에서 보낸 데이터 존재
+    console.log('Message handled in the background!', remoteMessage);
+});
 
 AppRegistry.registerComponent(appName, () => App);
